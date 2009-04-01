9cf225eca437eef741be5ccc3a99fd7ecb974426
Kevin Atkinson <kevina@cs.utah.edu>
2009-03-31 18:30:16 -0600

    Add missing file.
    svn-id: zl/trunk@206

---
+++ test/test69.c
@@ -0,0 +1,14 @@
+
+int main() {
+  char c = 1;
+  signed char sc = 2;
+  unsigned char uc = 3;
+  short s = 4;
+  unsigned short us = 5;
+  int i = 6;
+  unsigned u = 7;
+  long l = 8;
+  unsigned long lu = 9;
+  long long ll = 10;
+  unsigned long long ull = 10;
+}
