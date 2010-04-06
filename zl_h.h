2650351078a951f4bffa1f6669014ee1e0ac2ee6
Kevin Atkinson <kevina@cs.utah.edu>
2010-04-05 19:30:29 -0600

    Various enhancements fixes to zlc.
    svn-id: trunk/zl@577

--- zl_h.h
+++ zl_h.h
@@ -1,6 +1,14 @@
-#define __extension__
-#define __inline inline
+/* We do not suport many GNU extensions so let's not pretend we do */
+#undef __GNUC__
+#undef __GNUC_MINOR__
+#undef __GNUC_PATCHLEVEL__
+#undef __GNUG__
+
 #define __inline__ inline
-#define __const const
-#define __attribute__(parm)
-#define __restrict restrict
+
+/* #define __extension__ */
+/* #define __inline inline */
+/* #define __const const */
+/* #define __attribute__(parm) */
+/* #define __restrict restrict */
+/* #define __asm__(parm) */
